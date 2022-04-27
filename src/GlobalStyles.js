be42fb0aa16315df0750c1444f29a4b6eaaba7ed
osamamammar <osamaammar29@gmail.com>
2022-04-27 06:11:42 +0200

    build structure and install dependencies

---
+++ src/GlobalStyles.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+:root{
+ 
+}
+
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body, h1, h2, h3, h4, h5, h6, p, ol, ul, label {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+  font-family: "Roboto", sans-serif;
+  overflow-wrap: break-word;
+
+}
+
+body{
+}
+
+ol, ul {
+  list-style: none;
+}
+
+a{
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+.visually-hidden:not(:focus):not(:active) {
+  clip: rect(0 0 0 0);
+  clip-path: inset(100%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
+
+`;
+
+export default GlobalStyles;
